0487f087a9dbb94b2b39415aeb77bfe3bc67c16f
Xgt72 <xageorget@gmail.com>
2023-05-18 16:19:12 +0200

    vite-plugin-glsl added in frontend to use .glsl file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import glsl from "vite-plugin-glsl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), glsl()],
   resolve: {
     alias: {
       "@assets": path.resolve(__dirname, "src/assets"),
